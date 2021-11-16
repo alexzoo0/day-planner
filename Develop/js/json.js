fbdd7b0f7dab74f6b57869421365c45fa8e58e8d
alexzoo0 <alexisvelasquez15@gmail.com>
2021-11-16 17:31:02 -0600

    added time and set time blocks

---
+++ Develop/js/json.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+function workCalendar() {
+    $(document).ready(function () {
+        let time = moment().format('dddd, MMMM Do, YYYY h:mm a');
+        let workHours = ["7 a.m.", "8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.", "12 a.m.", "1 p.m.","2 p.m.", "3 p.m.", "4 p.m.", "5 p.m.", "6 p.m.", "7 p.m."];
+
+        function todayIs() {
+            let Current = $("#currentDay").text(time);
+            return Current;
+        }
+        todayIs();
+
+        
+
+        for (i = 0; i < workHours.length; i++) {
+            let row = $("<div>").addClass("row");
+            let column = $("<text-area>").text(workHours[i]).addClass("hour");
+            let inputClo = $("<input>").attr("placeholder", "Enter note here").addClass("toDo-input time-block");
+            saveBtn = $("<button>").addClass("btn btn-primary saveBtn").text("save");
+            $(row).append(column).append(inputClo).append(saveBtn);
+            $("#timeblock").append(row);
+        }
+
+        function addInput() {
+            let idInput = document.getElementsByClassName("toDo-input");
+            let length = idInput.length;
+            for (i = 0; i < length; i++) {
+                idInput[i].id = "input-field-" + (i + 1);
+            }
+        };
+        addInput();
+
+        function saveId() {
+            let saveB = document.getElementsByClassName("saveBtn");
+            let length = saveB.length;
+            for (i = 0; i < length; i++) {
+                saveB[i].id = "saveBtn-Id-" + (i + 1);
+            }
+        };
+        saveId();
+
+        
+    
+    });
+};
+workCalendar();
+
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# Work Day Scheduler Starter Code
+
+- you can visuallly see color change on timeblocks based on current time.
+- you can save content even when refreshing.
+- works well even for myself.
+
+# live version below 
+
+GIT PAGE LINK: https://alexzoo0.github.io/work-schedule/
+
+
+# images
+
+![Web screenshot](pic-1.png)
+![Web screenshot](pic-2.png)
